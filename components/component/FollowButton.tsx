a9c9c25cc588cd93dc8da3faa692d8cb1e02faa7
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2025-02-02 23:05:06 +0900

    follow

---
+++ components/component/FollowButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../ui/button"
+
+const FollowButton = () => {
+    return (
+        <div>
+            <Button className="w-full">Follow</Button>
+        </div>
+    )
+}
+
+export default FollowButton
